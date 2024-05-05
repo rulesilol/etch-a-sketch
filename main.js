d958e0695592d18634528fff7cc1190ff088a5dc
rulesilol <andrewlei323@gmail.com>
2024-05-05 20:54:26 +1000

    Added opacity and darkening

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 const generateButton = document.querySelector("#generate")
-const inputBox = document.querySelector("#length")
+const inputBox = document.getElementById("length")
 const gridDiv = document.querySelector(".gridDiv")
+let inputSideLength = parseInt(inputBox.value)
 
 function setEventListeners(widthHeightString) {
     const squares = document.querySelectorAll(".square")
     for (i = 0; i < squares.length; i++) {
         let currentSquare = squares[i]
         squares[i].addEventListener("mouseover", () => {
-            let backgroundStyle = getRandomColorStyle()
-            currentSquare.setAttribute("style", backgroundStyle + widthHeightString)
+            let currentOpacity = parseFloat(getComputedStyle(currentSquare).opacity)
+            let newOpacity = currentOpacity + 0.1
+            currentSquare.setAttribute("style", "background: black;" + widthHeightString + `opacity: ${newOpacity};`)
         })
         2
         squares[i].addEventListener("mouseout", () => {
-            currentSquare.setAttribute("style", "background: white;" + widthHeightString)
+            let currentOpacity = parseFloat(getComputedStyle(currentSquare).opacity)
+            currentSquare.setAttribute("style", "background: black;" + widthHeightString + `opacity: ${currentOpacity};`)
         })
 
     }
 }
 
-function getRandomColorStyle() {
-    color = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
-    return `background: ${color};`
-}
-
-function generateGrid() {
-    const sideLength = (inputBox.value)
+function generateGrid(sideLength) {
+    console.log(typeof sideLength)
+    if ((sideLength == 0) || (sideLength == "NaN")) {
+        return window.alert("Write a side length")
+    }
     if (sideLength > 100) {
         return window.alert("Input a number less than 100")
     }
     gridDiv.innerHTML = ""
-    let widthHeightString = `width : ${600/sideLength}px; height: ${600/sideLength}px`
+    let widthHeightString = `width : ${600/sideLength}px; height: ${600/sideLength}px;`
     console.log(widthHeightString)
     for (i = 0; i < sideLength; i++) {
         let newRow = document.createElement("div")
@@ -38,7 +39,7 @@ function generateGrid() {
         for (j = 0; j < sideLength; j++) {
             newSquare = document.createElement("div")
             newSquare.setAttribute("class", "square")
-            newSquare.setAttribute("style", widthHeightString)
+            newSquare.setAttribute("style", widthHeightString + "background:black; opacity: 0.0")
             newRow.appendChild(newSquare)
         }
     }
@@ -46,7 +47,15 @@ function generateGrid() {
     inputBox.focus()
 }
 
-generateButton.addEventListener("click", generateGrid)
+generateGrid(4)
+generateButton.addEventListener("click", () => {
+    integer = parseInt(inputBox.value)
+    if (isNaN(integer)) {
+        console.log("Not a number")
+    } else {
+        generateGrid(integer)
+    }
+})
 
-inputBox.focus()
-setEventListeners()
+
+// console.log(inputSideLength)
