e0e7ea3240c7c062e078ef9a4ea0aa01dc098156
rulesilol <andrewlei323@gmail.com>
2024-05-05 16:43:22 +1000

    Done extra credit 1

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-const squares = document.querySelectorAll(".square")
 const generateButton = document.querySelector("#generate")
 const inputBox = document.querySelector("#length")
 const gridDiv = document.querySelector(".gridDiv")
 
-for (i = 0; i < squares.length; i++) {
-    const currentSquare = squares[i]
-    squares[i].addEventListener("mouseover", () => {
-        // console.log("Hello")
-        currentSquare.setAttribute("style", "background: blue;")
-    })
-    2
-    squares[i].addEventListener("mouseout", () => {
-        currentSquare.setAttribute("style", "background: white")
-    })
+function setEventListeners(widthHeightString) {
+    const squares = document.querySelectorAll(".square")
+    for (i = 0; i < squares.length; i++) {
+        let currentSquare = squares[i]
+        squares[i].addEventListener("mouseover", () => {
+            let backgroundStyle = getRandomColorStyle()
+            currentSquare.setAttribute("style", backgroundStyle + widthHeightString)
+        })
+        2
+        squares[i].addEventListener("mouseout", () => {
+            currentSquare.setAttribute("style", "background: white;" + widthHeightString)
+        })
 
+    }
+}
+
+function getRandomColorStyle() {
+    color = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
+    return `background: ${color};`
+}
+
+function generateGrid() {
+    const sideLength = (inputBox.value)
+    if (sideLength > 100) {
+        return window.alert("Input a number less than 100")
+    }
+    gridDiv.innerHTML = ""
+    let widthHeightString = `width : ${600/sideLength}px; height: ${600/sideLength}px`
+    console.log(widthHeightString)
+    for (i = 0; i < sideLength; i++) {
+        let newRow = document.createElement("div")
+        newRow.setAttribute("class", "rows")
+        gridDiv.appendChild(newRow)
+        for (j = 0; j < sideLength; j++) {
+            newSquare = document.createElement("div")
+            newSquare.setAttribute("class", "square")
+            newSquare.setAttribute("style", widthHeightString)
+            newRow.appendChild(newSquare)
+        }
+    }
+    setEventListeners(widthHeightString)
+    inputBox.focus()
 }
 
 generateButton.addEventListener("click", generateGrid)
 
-function generateGrid() {
-    const sideLength = (inputBox.value)
-    for (i = 0; i < sideLength; i++) {
-        let newRow = document.createElement("div")
-        
-        for (j = 0; j < sideLength; j ++) {
-
-        }
-    }
-}
+inputBox.focus()
+setEventListeners()
